66201f6a2f98b089c45cd1fcf0191143d08a1613
Eylon <eylon.perets@gmail.com>
2022-02-27 12:00:49 +0200

    a

---
+++ p9/app.js
@@ -0,0 +1,6 @@
+const items = [];
+const tshirt = { name: 'tshirt', price: 25, color: 'green' };
+const hoodie = { name: 'hoodie', price: 40, color: 'olive' };
+const jeans = { name: 'jeans', price: 50, color: 'blue' };
+items.push(tshirt, hoodie, jeans);
+console.log(items);
