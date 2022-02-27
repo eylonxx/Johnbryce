a504fba6af88498c5ecc2ebcbac490279df8a0d6
Eylon <eylon.perets@gmail.com>
2022-02-27 15:03:33 +0200

    o

--- p9/app.js
+++ p9/app.js
@@ -1,6 +1,24 @@
 const items = [];
-const tshirt = { name: 'tshirt', price: 25, color: 'green' };
-const hoodie = { name: 'hoodie', price: 40, color: 'olive' };
-const jeans = { name: 'jeans', price: 50, color: 'blue' };
+const tshirt = { name: 'tshirt', price: 25, img: './images.jpg', color: 'green' };
+const hoodie = { name: 'hoodie', price: 40, img: './hoodie.jpg', color: 'olive' };
+const jeans = { name: 'jeans', price: 50, img: './jeans.jpg', color: 'blue' };
 items.push(tshirt, hoodie, jeans);
 console.log(items);
+
+const cart = [];
+const itemList = document.querySelector('.d-flex');
+
+for (let i = 0; i < items.length; i++) {
+  itemList.innerHTML += `<div class="card" style="width: 18rem;">
+<img src="${items[i].img}" class="card-img-top" alt>
+<div class="card-body">
+  <p class="card-text">${items[i].name}, ${items[i].price}, ${items[i].color}</p>
+  <button onclick="addItem(${i})">Buy</button>
+  <button>Remove</button>
+</div>
+</div>`;
+}
+function addItem(i) {
+  cart.push(items[i]);
+  console.log(cart);
+}
