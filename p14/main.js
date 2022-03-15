a688a74237ef00c7e9e62c88fa77bf89c000b368
Eylon <eylon.perets@gmail.com>
2022-03-15 16:14:51 +0200

    Promises

--- p14/main.js
+++ p14/main.js
@@ -14,26 +14,89 @@
 // };
 
 //2nd exercise
-const getRandomBrightColorAfterDelayTEST = () => {
-  getRandomBrightColorAfterDelay(
-    (selectedColor) => {
-      document.body.style.backgroundColor = selectedColor;
-    },
-    (err) => {
-      alert('not a bright color!!');
-    }
-  );
+// const getRandomBrightColorAfterDelayTEST = () => {
+//   getRandomBrightColorAfterDelay(
+//     (selectedColor) => {
+//       document.body.style.backgroundColor = selectedColor;
+//     },
+//     (err) => {
+//       alert('not a bright color!!');
+//     }
+//   );
+// };
+
+// const getRandomBrightColorAfterDelay = (succCallback, errCallback) => {
+//   setTimeout(() => {
+//     const colors = ['Yellow', 'Black', 'LightBlue', 'Brown', 'LightGreen', 'DarkMagenta'];
+//     const num = Math.floor(Math.random() * colors.length);
+//     const selectedColor = colors[num];
+//     if (selectedColor === 'LightBlue' || selectedColor === 'Yellow' || selectedColor === 'LightGreen') {
+//       succCallback(selectedColor);
+//     } else {
+//       errCallback();
+//     }
+//   }, 3000);
+// };
+
+// function getCat() {
+//   const cat = {
+//     name: 'mitsi',
+//     age: 4,
+//   };
+// }
+
+// console.log(getCat().name);
+
+// //3rd exercise - promises
+// const getRandomBrightColorAfterDelayTEST = () => {
+//   getRandomBrightColorAfterDelay()
+//     .then((selectedColor) => {
+//       document.body.style.backgroundColor = selectedColor;
+//     })
+//     .catch((err) => {
+//       alert('not a bright color!!');
+//     });
+// };
+
+// const getRandomBrightColorAfterDelay = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const colors = ['Yellow', 'Black', 'LightBlue', 'Brown', 'LightGreen', 'DarkMagenta'];
+//       const num = Math.floor(Math.random() * colors.length);
+//       const selectedColor = colors[num];
+//       if (selectedColor === 'LightBlue' || selectedColor === 'Yellow' || selectedColor === 'LightGreen') {
+//         resolve(selectedColor);
+//       } else {
+//         reject();
+//       }
+//     }, 3000);
+//   });
+// };
+
+//4th exercise
+
+const randomDiscount = () => {
+  const min = +prompt('enter the minimum number: ');
+  const max = +prompt('enter the maximum number: ');
+  getRandomSmallDiscountAfterDelay(min, max)
+    .then((randomPrice) => {
+      console.log('nice!', randomPrice);
+    })
+    .catch((randomPrice) => {
+      console.log('oh no!', randomPrice);
+    });
 };
 
-const getRandomBrightColorAfterDelay = (succCallback, errCallback) => {
-  setTimeout(() => {
-    const colors = ['Yellow', 'Black', 'LightBlue', 'Brown', 'LightGreen', 'DarkMagenta'];
-    const num = Math.floor(Math.random() * colors.length);
-    const selectedColor = colors[num];
-    if (selectedColor === 'LightBlue' || selectedColor === 'Yellow' || selectedColor === 'LightGreen') {
-      succCallback(selectedColor);
-    } else {
-      errCallback();
-    }
-  }, 3000);
+const getRandomSmallDiscountAfterDelay = (min, max) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      randomPrice = Math.floor(Math.random() * (max - min + 1) + min);
+      randomAverage = (max + min) / 2;
+      if (randomPrice > randomAverage) {
+        resolve(randomPrice);
+      } else {
+        reject(randomPrice);
+      }
+    }, 3000);
+  });
 };
