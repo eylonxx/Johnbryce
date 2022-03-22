584fa863ebf29ecd4e233ec69b75d03900440b66
Eylon <eylon.perets@gmail.com>
2022-03-22 10:43:12 +0200

    class exercise

---
+++ p16/main.js
@@ -0,0 +1,15 @@
+const test = () => {
+  try {
+    let final = factorGrade(105);
+    console.log(final);
+  } catch {
+    console.log('hillel');
+  }
+};
+
+const factorGrade = (grade) => {
+  const factoredGrade = 10 * Math.sqrt(grade);
+  if (factoredGrade < 0 || factoredGrade > 100) {
+    throw new Error();
+  } else return factoredGrade;
+};
