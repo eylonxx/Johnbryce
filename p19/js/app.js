44e10fe20b807d784b53566d2a508ef8feeb788e
eylonx <eylon.perets@gmail.com>
2022-03-29 16:11:26 +0300

    ts

--- p19/js/app.js
+++ p19/js/app.js
@@ -1,4 +1,39 @@
-var n1 = 10;
-var n2 = 20;
-var sum = n1 + n2;
-console.log(sum);
+// let i: number;
+// for (i = 100; i <= 200; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+// function printText(text: string, times: number) {
+//   for (let i = 1; i <= times; i++) {
+//     console.log(text);
+//   }
+// }
+// printText('hello', 3);
+// function getAverage(n1: number, n2: number): number {
+//   //must have a return
+//   const sum = (n1 + n2) / 2;
+//   //need to declare what a function returns
+//   return sum;
+// }
+// console.log(getAverage(3, 5));
+// //export and import works like react
+var randState = function () {
+    var states = ['America', 'Africa', 'Australia'];
+    return states[Math.floor(Math.random() * states.length)];
+};
+var Parrot = /** @class */ (function () {
+    function Parrot(name, color, breed) {
+        this.name = name;
+        this.color = color;
+        this.breed = breed;
+        this.origin = randState();
+    }
+    Parrot.prototype.display = function () {
+        console.log('my name is' + this.name);
+    };
+    Parrot.prototype.fly = function () {
+        console.log("".concat(this.name, " is flying"));
+    };
+    return Parrot;
+}());
+var parrot1 = new Parrot('oded', 'black', 'gay');
+console.log(parrot1);
--- p19/src/app.ts
+++ p19/src/app.ts
@@ -1,5 +1,50 @@
-const n1: number = 10;
-const n2: number = 20;
-const sum = n1 + n2;
+// let i: number;
+// for (i = 100; i <= 200; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
 
-console.log(sum);
+// function printText(text: string, times: number) {
+//   for (let i = 1; i <= times; i++) {
+//     console.log(text);
+//   }
+// }
+// printText('hello', 3);
+
+// function getAverage(n1: number, n2: number): number {
+//   //must have a return
+//   const sum = (n1 + n2) / 2;
+//   //need to declare what a function returns
+//   return sum;
+// }
+// console.log(getAverage(3, 5));
+
+// //export and import works like react
+const randState = (): string => {
+  const states: string[] = ['America', 'Africa', 'Australia'];
+  return states[Math.floor(Math.random() * states.length)];
+};
+class Parrot {
+  name: string;
+  color: string;
+  breed: string;
+  origin: string;
+
+  constructor(name: string, color: string, breed: string) {
+    this.name = name;
+    this.color = color;
+    this.breed = breed;
+    this.origin = randState();
+  }
+
+  display(): void {
+    console.log('my name is' + this.name);
+  }
+
+  fly(): void {
+    console.log(`${this.name} is flying`);
+  }
+}
+
+const parrot1 = new Parrot('oded', 'black', 'gay');
+
+console.log(parrot1);
