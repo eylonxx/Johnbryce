223d97369b081480746ebc702c48a33ddb9024f2
Eylon <eylon.perets@gmail.com>
2022-03-30 08:31:12 +0300

    project

--- p18 project2/main.js
+++ p18 project2/main.js
@@ -1,6 +1,6 @@
 $(function () {
   const data = $.ajax({
-    url: 'https://api.coingecko.com/api/v3/coins/list',
+    url: 'https://api.coingecko.com/api/v3/coins/',
     success: (response) => {
       storeData(response);
     },
@@ -19,7 +19,7 @@ $(function () {
             <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
             <label class="form-check-label" for="flexSwitchCheckDefault"></label>
           </div>
-          <h5 class="card-title">${data[i].name}</h5>
+          <h5 class="card-title">${data[i].symbol.toUpperCase()}</h5>
           <p class="card-text">${data[i].id}</p>
           <p>
           <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample${i}" aria-expanded="false" aria-controls="collapseExample">
@@ -28,7 +28,11 @@ $(function () {
           </p>
           <div class="collapse" id="collapseExample${i}">
             <div class="card card-body">
-              Some coin info </div>
+              <img src="${data[i].image.small}">
+              <span>${data[i].market_data.current_price.usd}</span
+              <span>${data[i].market_data.current_price.eur}</span
+              <span>${data[i].market_data.current_price.ils}</span
+              </div>
           </div>
         </div>
       </div>
