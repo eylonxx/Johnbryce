5a8786f4f2b23f52df6ba364f3bbf28274eaa3b2
Eylon <eylon.perets@gmail.com>
2022-04-26 11:29:00 +0300

    region update

--- p23 test/main.js
+++ p23 test/main.js
@@ -1,9 +1,11 @@
 const allButton = document.querySelector('#all');
-const searchButton = document.querySelector('#searchButton');
 const searchInput = document.querySelector('#searchInput');
 const resultsContainer = document.querySelector('#results');
 const resultsCountry = document.querySelector('#results-country');
 const resultsRegion = document.querySelector('#results-region');
+const form = document.querySelector('#form');
+
+// EYLON PERETS 318245453
 
 const getData = async (search) => {
   const data = fetch(`https://restcountries.com/v3.1/${search}`).then((response) => response.json());
@@ -23,7 +25,7 @@ const generateTemplate = (data) => {
   resultsCountry.innerHTML = `
 <h2>Total Population: ${total}</h2>
 <h2>Average Population: ${average}</h2>
-<table>
+<table class="table table-light">
 <thead>
     <tr>
         <th>Country Name</th>
@@ -39,7 +41,6 @@ const generateTemplate = (data) => {
   //fill country info
   const countryContent = document.querySelector('#country-content');
   for (let country of data) {
-    console.log(country.region);
     countryContent.innerHTML += `
   <tr>
   <td>${country.name.official}</td>
@@ -65,70 +66,22 @@ const generateTemplate = (data) => {
   //
 
   const regionContent = document.querySelector('#region-content');
-  let Americas = 0;
-  let Africa = 0;
-  let Asia = 0;
-  let Oceania = 0;
-  let Antarctic = 0;
+  const regions = { Americas: 0, Africa: 0, Asia: 0, Oceania: 0, Antarctic: 0 };
+
   for (let country of data) {
-    switch (country.region) {
-      case 'Americas':
-        Americas++;
-        break;
-      case 'Africa':
-        Africa++;
-        break;
-      case 'Asia':
-        Asia++;
-        break;
-      case 'Oceania':
-        Oceania++;
-        break;
-      case 'Antarctic':
-        Antarctic++;
+    regions[country.region]++;
+  }
+
+  Object.entries(regions).forEach((entry) => {
+    if (entry[1]) {
+      regionContent.innerHTML += `
+        <tr>
+        <td>${entry[0]}</td>
+        <td>${entry[1]}</td>
+        </tr>
+        `;
     }
-  }
-  //0 is not truthy, will only display if >0
-  if (Americas) {
-    regionContent.innerHTML += `
-  <tr>
-  <td>Americas</td>
-  <td>${Americas}</td>
-  </tr>
-  `;
-  }
-  if (Africa) {
-    regionContent.innerHTML += `
-  <tr>
-  <td>Africa</td>
-  <td>${Africa}</td>
-  </tr>
-  `;
-  }
-  if (Asia) {
-    regionContent.innerHTML += `
-  <tr>
-  <td>Asia</td>
-  <td>${Asia}</td>
-  </tr>
-  `;
-  }
-  if (Oceania) {
-    regionContent.innerHTML += `
-  <tr>
-  <td>Oceania</td>
-  <td>${Oceania}</td>
-  </tr>
-  `;
-  }
-  if (Antarctic) {
-    regionContent.innerHTML += `
-  <tr>
-  <td>Antarctic</td>
-  <td>${Antarctic}</td>
-  </tr>
-  `;
-  }
+  });
 };
 
 const resetPage = () => {
@@ -141,22 +94,23 @@ allButton.addEventListener('click', async () => {
   resetPage();
   try {
     const data = await getData('all');
-    console.log(data);
     generateTemplate(data);
+    searchInput.value = '';
   } catch (e) {
-    resultsContainer.innerHTML = 'ERROR! THIS COUNTRY DOES NOT EXIST!';
+    resultsCountry.innerHTML = 'ERROR! THIS COUNTRY DOES NOT EXIST!';
     console.log(e);
   }
 });
-
-//get Specific country
-searchButton.addEventListener('click', async () => {
+//form submit
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
   resetPage();
   try {
     const data = await getData(`/name/${searchInput.value}`);
     generateTemplate(data);
+    searchInput.value = '';
   } catch (e) {
-    resultsContainer.innerHTML = 'ERROR! THIS COUNTRY DOES NOT EXIST!';
+    resultsCountry.innerHTML = 'ERROR! THIS COUNTRY DOES NOT EXIST!';
     console.log(e);
   }
 });
