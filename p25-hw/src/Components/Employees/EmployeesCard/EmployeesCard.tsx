fcc16b6b5513d05b56634176e54437e94ed91a37
Eylon <eylon.perets@gmail.com>
2022-05-14 17:09:42 +0300

    no subscribe

--- p25-hw/src/Components/Employees/EmployeesCard/EmployeesCard.tsx
+++ p25-hw/src/Components/Employees/EmployeesCard/EmployeesCard.tsx
@@ -1,16 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import EmployeeModel from '../../../Models/emoloyeeModel';
+import employeeService from '../../../Services/EmployeeService';
 
 interface EmployeeCardProps {
   employee: EmployeeModel;
 }
 
 function EmployeeCard(props: EmployeeCardProps): JSX.Element {
+  const navigate = useNavigate();
+  async function deleteEmployee() {
+    try {
+      await employeeService.deleteEmployee(props.employee.id);
+      alert('employee has been deleted.');
+      navigate('/employees');
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
   return (
     <div className="">
       <h4>
         {props.employee.firstName} {props.employee.lastName}
         <NavLink to={`/employees/${props.employee.id}`}>edit</NavLink>
+        <button onClick={deleteEmployee}>delete</button>
       </h4>
     </div>
   );
