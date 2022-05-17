3b654c3310349f75bc5cc194606a6c5603670387
Eylon <eylon.perets@gmail.com>
2022-05-17 16:30:06 +0300

    fix run script

--- p27/src/app.ts
+++ p27/src/app.ts
@@ -1,3 +1,21 @@
+import express from 'express';
+const server = express();
+
+server.listen(3001, () => {
+  console.log('running on port 3001!');
+});
+
+server.get('/api/kittens', (req, res) => {
+  res.send('lol');
+});
+
+server.get('/api/kittens/:id', (req, res) => {
+  const id = +req.params.id;
+  const kitten = kittens.find((k) => k.id === id);
+  res.json(kitten);
+  console.log(id);
+});
+
 const kittens = [
   { id: 1, name: 'Mitsi', age: 4 },
   { id: 2, name: 'Kitsi', age: 5 },
