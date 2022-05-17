5fc05f04ee69177d882ac6e5355e5fb1096a484c
Eylon <eylon.perets@gmail.com>
2022-05-17 11:32:08 +0300

    node js

---
+++ p26/app.js
@@ -0,0 +1,3 @@
+const cypher = require('./cypher.js');
+
+cypher.encrypt('hi');
---
+++ p26/cypher.js
@@ -0,0 +1,27 @@
+const key = 130;
+
+function encrypt(plainText) {
+  let cypherText = '';
+  for (let i = 0; i < plainText.length; i++) {
+    let ascii = plainText.charCodeAt(i);
+    ascii += key;
+    const char = String.fromCharCode(ascii);
+    cypherText += char;
+  }
+  return cypherText;
+}
+
+function decrypt(cypherText) {
+  for (let i = 0; i < cypherText.length; i++) {
+    let ascii = cypherText.charCodeAt(i);
+    ascii -= key;
+    const char = String.fromCharCode(ascii);
+    plainText += char;
+  }
+  return plainText;
+}
+
+module.exports = {
+  encrypt,
+  decrypt,
+};
