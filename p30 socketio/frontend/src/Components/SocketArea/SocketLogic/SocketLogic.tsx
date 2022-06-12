64db38d7cf30000230878533056ccfc6b3d8d1ba
Eylon <eylon.perets@gmail.com>
2022-06-12 16:44:39 +0300

    socketio shenanigans

--- p30 socketio/frontend/src/Components/SocketArea/SocketLogic/SocketLogic.tsx
+++ p30 socketio/frontend/src/Components/SocketArea/SocketLogic/SocketLogic.tsx
@@ -4,15 +4,13 @@ import socketService from '../../../Services/SocketService';
 
 export default function SocketLogic() {
   function connect(): void {
-    socketService.connect(displayNumberSentFromServer);
+    socketService.connect();
   }
   function send(): void {
     const msgObj: MessageModel = { title: 'lol', text: 'xd' };
     socketService.send(msgObj);
   }
-  function displayNumberSentFromServer(num: number): void {
-    console.log('num is' + num);
-  }
+
   function disconnect(): void {
     console.log('disconnected!');
     socketService.disconnect();
--- p30 socketio/frontend/src/Services/SocketService.ts
+++ p30 socketio/frontend/src/Services/SocketService.ts
@@ -3,8 +3,9 @@ import MessageModel from '../Models/messageModel';
 
 class SocketService {
   private socket: Socket;
-  public connect(callback: Function): void {
+  public connect(): void {
     this.socket = io('http://localhost:3001');
+    console.log('connected');
 
     this.socket.on('msg', (msgObj) => {
       console.log(`got a message, ${msgObj}`);
@@ -14,7 +15,7 @@ class SocketService {
     this.socket.emit('msg', msgObj);
   }
   public disconnect(): void {
-    this.socket.emit('disconnect');
+    this.socket.disconnect();
   }
 }
 const socketService = new SocketService();
