46f680aeab4a5ff876c439e874c5eca97ab21b1a
Eylon <eylon.perets@gmail.com>
2022-07-03 09:31:36 +0300

    angular setup

---
+++ p32 frontend auth/Secure Coding/controllers/products-controller.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const productsLogic = require("../business-logic/products-logic");
+const router = express.Router();
+
+router.get("/", async (request, response) => {
+    try {
+        const products = await productsLogic.getAllProductsAsync();
+        response.json(products);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+module.exports = router;
